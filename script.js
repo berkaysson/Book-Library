052ace329a7d08a3483965781688542232ff44ed
berkay sonel <berkaysonel85@gmail.com>
2022-10-29 21:18:25 +0300

    To clear input fields after editting or adding a book, function added. To validate if form input fields are filled, for loops added to editBook and addBook functions.

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 let library = [];
 const library_section = document.getElementById("library");
-const editBook_title = document.getElementById("editBook-title");
+
+const editBook_header = document.getElementById("editBook-header");
+
+const book_title = document.querySelector("input[name='addBook-title']");
+const book_author = document.querySelector("input[name='addBook-author']");
+const book_pages = document.querySelector("input[name='addBook-pages']");
+const book_read = document.querySelector("input[name='addBook-read']");
+
+const $input = document.querySelectorAll("[data-type]");
+
 let book_counter = 1;
 
 function Books(title, author, pages, read, book_id){
@@ -18,7 +27,7 @@ Books.prototype.info = function(){
 function openForm(form_name, edit_id) {
     document.getElementById(`${form_name}Book-popup`).style.display = "block";
     if (form_name === "edit") {
-        editBook_title.textContent = library[library.indexOf(library.find(x => x.id === edit_id))].title;
+        editBook_header.textContent = library[library.indexOf(library.find(x => x.id === edit_id))].title;
         document.getElementsByName("editBook-button")[0].setAttribute("onclick", `editBook(${edit_id})`);
     }
     document.querySelectorAll(".filtered").forEach(el => {
@@ -36,42 +45,23 @@ function closeForm(form_name) {
 // addBook(), creates new Object with input values and book_counter value and pushes that object to library array,
 // call displayLibrary which shows all Books objects in library array
 
-function addBook(){ 
-    library.push(new Books(
-        document.getElementsByName("book-title")[0].value, 
-        document.getElementsByName("book-author")[0].value, 
-        document.getElementsByName("book-pages")[0].value, 
-        document.getElementsByName("book-read")[0].value, 
+function addBook(){
+    for (el of $input) {
+        if (el.getAttribute("data-type") == "add") {
+            if (el.value == "") {
+                return alert(`Please fill ${el.getAttribute("placeholder")} field!`);
+            }
+        }
+    }
+    library.push(Book = new Books(
+        book_title.value, 
+        book_author.value, 
+        book_pages.value, 
+        book_read.value, 
         book_counter));
     displayLibrary();
+    clearFields();
     book_counter++;
-    document.getElementsByName("book-title")[0].value = "", 
-    document.getElementsByName("book-author")[0].value = "", 
-    document.getElementsByName("book-pages")[0].value = "",
-    document.getElementsByName("book-read")[0].value="Not Readed"; 
-}
-
-function displayLibrary() {
-    library_section.textContent = " ";
-    for (let book of library) {
-        const book_card = 
-        `
-            <article class="book">
-            <img src="images/Book_Covers/cover (${book.id % 7}).png" alt="">
-            <div class="book-info">
-                <p class="book-title">Title of book : ${book.title}</p>
-                <p>Author of book : ${book.author}</p>
-                <p>Pages : ${book.pages}</p>
-                <p>Did you read this book : ${book.read}</p>
-                <div class="btn-container">
-                    <button class="btn" onclick="openForm(\`edit\`,${book.id})">Edit</button>
-                    <button class="btn red-btn" id="book-${book.id}" onclick="deleteBook(${book.id})">Delete</button>
-                </div>
-            </div>
-            </article>
-    `;
-    library_section.insertAdjacentHTML("beforeend", book_card);
-    }
 }
 
 function deleteBook(delete_id) {
@@ -79,19 +69,55 @@ function deleteBook(delete_id) {
     displayLibrary();
 }
 
-function toggle(button) {
-    button.value=(button.value=="Readed")?"Not Readed":"Readed";
-}
-
 function editBook(edit_id) {
+    for (el of $input) {
+        if (el.getAttribute("data-type") == "edit") {
+            if (el.value == "") {
+                return alert(`Please fill ${el.getAttribute("placeholder")} field!`);
+            }
+        }
+    }
     library[library.indexOf(library.find(x => x.id === edit_id))].title = document.getElementsByName("editBook-title")[0].value;
     library[library.indexOf(library.find(x => x.id === edit_id))].author = document.getElementsByName("editBook-author")[0].value;
     library[library.indexOf(library.find(x => x.id === edit_id))].pages = document.getElementsByName("editBook-pages")[0].value;
     library[library.indexOf(library.find(x => x.id === edit_id))].read = document.getElementsByName("editBook-read")[0].value;
-    document.getElementsByName("editBook-title")[0].value = "", 
-    document.getElementsByName("editBook-author")[0].value = "", 
-    document.getElementsByName("editBook-pages")[0].value = "",
-    document.getElementsByName("editBook-read")[0].value="Not Readed"; 
+    clearFields();
     displayLibrary();
     closeForm("edit");
+}
+
+function toggle(button) {
+    button.value=(button.value=="Readed")?"Not Readed":"Readed";
+}
+
+function clearFields() {
+    document.querySelectorAll("input").forEach(el => {
+        if (el.type == "button") {
+            el.value = "Not Readed";
+        }
+        else {
+            el.value = "";
+        }
+    });
+}
+
+function displayLibrary() {
+    library_section.textContent = ``;
+    for (let book of library) {
+        const book_card = 
+        `
+        <tr>
+        
+        <th><p class="book-title">${book.title}</p></th>
+        <th><p>${book.author}</p></th>
+        <th>${book.pages}</th>
+        <th>${book.read}</th>
+        <th><div class="btn-container">
+            <button class="btn" onclick="openForm(\`edit\`,${book.id})">Edit</button>
+            <button class="btn red-btn" id="book-${book.id}" onclick="deleteBook(${book.id})">Delete</button>
+        </div></th>
+    </tr>
+    `;
+    library_section.insertAdjacentHTML("beforeend", book_card);
+    }
 }
